b10c4e76db442f4d4c82bfebad7679fae33233c3
starkof <saystephan@gmail.com>
2018-09-13 15:43:07 +0000

    Completed my-cat

---
+++ Project 1/tomKitten.c
@@ -0,0 +1,30 @@
+// Simple implementation of the cat command in unix systems
+// The name of this program is inspired by The Tale of Tom Kitten and wordplay on cat
+// by: Stephan N. Ofosuhene
+
+#include <stdio.h>
+
+#define BUFFER_SIZE 255
+
+
+int main(int argc, char *argv[]){
+	if (argc == 1){
+		printf("Please provide the name of a file as a parameter %d\n", argc);
+		return 0;
+	} else if (argc == 2){
+		FILE *fp;
+		char buffer[BUFFER_SIZE];
+
+		fp = fopen(argv[1], "r");
+		if (fp == NULL){
+			printf("Cannot open file\n");
+			return -1;
+		}
+
+		while (fgets(buffer, BUFFER_SIZE, fp) != NULL){
+			printf("%s",sizeof(buffer)/sizeof(buffer[0]), buffer);
+		}
+		fclose(fp);
+	}
+	return 0;
+}
