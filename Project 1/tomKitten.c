96288fccf9d783f32eb03a8f3c61851b413fbf12
starkof <saystephan@gmail.com>
2018-09-20 12:07:11 +0000

    my-grep.c and tomKitten.c can now work with multiple files

--- Project 1/tomKitten.c
+++ Project 1/tomKitten.c
@@ -9,27 +9,29 @@ int main(int argc, char *argv[]){
 	if (argc == 1){
 		printf("Please provide the name of a file as a parameter \n");
 		return 0;
-	} else if (argc == 2){
-		FILE *fp;
+	} else if (argc >= 2) {
+        FILE *fp;
 
-		// open the file
-		fp = fopen(argv[1], "r");
+        for (int i = 1; i <= argc - 1; i++) {
+            // open the file
+            char *filename = argv[i];
+            printf("\n********* File %d: %s *********\n\n", i, filename);
+            fp = fopen(filename, "r");
 
-		// check if the file was opened successfully
-		if (fp == NULL){
-			printf("Cannot open file\n");
-			return -1;
-		}
+            // check if the file was opened successfully
+            if (fp == NULL) {
+                printf("Cannot open file\n");
+                return -1;
+            }
 
-		char c;
-		do {
-			c = fgetc(fp);
-			printf("%c", c);
-		} while (c != EOF);
+            char c;
+            do {
+                c = fgetc(fp);
+                printf("%c", c);
+            } while (c != EOF);
 
-		fclose(fp);
-	} else {
-		printf("Too many arguments have been provided for this program\n");
-	}
+            fclose(fp);
+        }
+    }
 	return 0;
 }
